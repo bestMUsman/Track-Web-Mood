466812110a488a08584d93dd041965500bc05090
Muhammad Usman <musmanraodev@gmail.com>
2017-05-07 22:57:15 -0400

    finished

--- db/migrations/migration_04222017.sql
+++ db/migrations/migration_04222017.sql
@@ -1,10 +1,6 @@
 CREATE TABLE IF NOT EXISTS webdata (
   id BIGSERIAL PRIMARY KEY,
   urllink VARCHAR(1024),
-  anger VARCHAR(255),
-  disgust VARCHAR(255),
-  fear VARCHAR(255),
-  joy VARCHAR(255),
-  sadness VARCHAR(255),
+  name VARCHAR(1024),
   user_id INTEGER REFERENCES users(id)
 );

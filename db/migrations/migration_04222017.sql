a17b416b7332e114f069537e7b2d3e997f83236f
Muhammad Usman <musmanraodev@gmail.com>
2017-07-03 23:06:36 -0400

    updated sql

--- db/migrations/migration_04222017.sql
+++ db/migrations/migration_04222017.sql
@@ -1,7 +1,16 @@
 DROP TABLE webdata;
+DROP TABLE users;
 
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  password TEXT UNIQUE NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS webdata (
+CREATE TABLE webdata (
   id BIGSERIAL PRIMARY KEY,
   urllink VARCHAR(1024),
   description VARCHAR(1024),
